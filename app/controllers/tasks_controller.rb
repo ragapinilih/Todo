2783b19f4773140d960132c657360337de1db542
Raga Pinilih <ragapinilih@gmail.com>
2014-03-10 19:20:17 +0700

    Part Two

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
 	def new
 		@task = Task.new
+		render :show_form
 	end	
 
 	def create
 		@task = Task.create(task_params)
-		@tasks = Task.all
+		save_task
 	end
 
 	def destroy
@@ -13,6 +14,27 @@ class TasksController < ApplicationController
 		@task.destroy
 		@tasks = Task.all		
 	end
+
+	def edit
+		@task = Task.find(params[:id])
+		render :show_form
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		@task.update_attributes(task_params)	
+		save_task
+	end
+
+	def save_task
+		if @task.save
+			@tasks = Task.all
+			render :hide_form
+		else
+			render :show_form
+		end	
+	end
+
 	private
 		def task_params
 			params.require(:task).permit(:title, :note, :completed)
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+Date::DATE_FORMATS[:default]="%d/%m/%Y"
 # Initialize the Rails application.
 Todo::Application.initialize!
